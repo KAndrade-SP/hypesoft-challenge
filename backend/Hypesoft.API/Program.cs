6473f316327e9f55c168b3e498088960b1564e0b
KAndrade-SP <kaique.viana090@gmail.com>
2026-01-16 17:43:13 -0300

    fix: resolve swagger openapi version conflict on .net 9

--- backend/Hypesoft.API/Program.cs
+++ backend/Hypesoft.API/Program.cs
@@ -3,16 +3,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
